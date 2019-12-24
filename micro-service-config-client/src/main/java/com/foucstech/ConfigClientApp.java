fcc703166bdb47026307465c79c106871192d7ed
wuandong <wuandong@focuschina.com>
2019-12-24 14:23:48 +0800

    commit

---
+++ micro-service-config-client/src/main/java/com/foucstech/ConfigClientApp.java
@@ -0,0 +1,14 @@
+package com.foucstech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class ConfigClientApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApp.class, args);
+    }
+}
