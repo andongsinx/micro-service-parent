1de3c4d3b48bc2e2749174340822839b3809a15b
wuandong <wuandong@focuschina.com>
2019-12-24 19:21:00 +0800

    commit

---
+++ micro-service-api-gateway/src/main/java/com/foucstech/TokenFilter.java
@@ -0,0 +1,35 @@
+package com.foucstech;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by wuandong on 2019/12/24.
+ */
+@Component
+public class TokenFilter extends ZuulFilter {
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        System.out.println("2222222222222");
+
+        return null;
+    }
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
+    }
+}
